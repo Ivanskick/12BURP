65acd8ca9e3ddf0e47f3a4e450b28b7429d43bd1
Ivanskcik <i.shkodrova2017@pmg-pernik.com>
2021-10-14 13:45:50 +0300

    zadacha1

---
+++ CRUD_ORM/CRUD_ORM/Data/Models/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_ORM.Data.Models
+{
+    class ProductContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =.; Integrted Sequrity = true; Database=ProductDb");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
